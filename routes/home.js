8fc69b40a03bcc92f361ce22d5e464cdf6939b98
JmPotato <ghzpotato@gmail.com>
2019-03-06 16:23:44 +0800

    Update HTML files

--- routes/home.js
+++ routes/home.js
@@ -8,10 +8,6 @@ var api_url = config.api_url;
 app.get('/', function(req, res) {
     if (req.cookies.user) {
         request.get({url: api_url + '/users/timeline', headers: {'user_id': JSON.parse(req.cookies.user).id}}, function(error, response, body) {
-            if (body === '[]') {
-                res.redirect('/user/signout');
-                return;
-            }
             var entries = JSON.parse(body);
             res.render('timeline', {page_title: "首页", entries});
         });
