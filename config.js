12fb9456805549e9ee47c92241c50fb832e34da8
JmPotato <ghzpotato@gmail.com>
2019-03-15 10:48:29 +0800

    Add config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = {
     server_url: 'http://localhost:8080/',
     server_port: 8080,
-    api_url: '',
+    api_url: 'http://39.105.127.55:3000',
     key: 'PLEASE CHANGE THIS!!!',
 }
 
